f8ae0c69e548d538e792652f12c026b3f608fc2b
Reinel Arteaga <reinel.artega.trabajo@gmail.com>
2021-06-17 17:15:50 -0400

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import { lazy, Suspense } from 'react';
+
 import './App.scss';
 
 
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import PrivateRoute from './components/Atoms/Router/PrivateRoute';
+
+
 
 //Screens
-import Login from './components/Screens/Login';
-import Bookings from './components/Screens/Bookings';
-import Hour from './components/Screens/Hour';
+const Login = lazy(() => import("./components/Screens/Login"));
+const Bookings = lazy(() => import('./components/Screens/Bookings'))
+const Hour = lazy(() => import('./components/Screens/Hour'))
 
-import PrivateRoute from './components/Atoms/Router/PrivateRoute';
+
+
 
 function App() {
   return (
 
     <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/hour" exact component={Hour} />
-        <PrivateRoute path="/bookings" exact component={Bookings} />
-        <Redirect to="/" />
-      </Switch>
+      <Suspense fallback={ <p>Espere...</p> } >
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/hour" exact component={Hour} />
+          <PrivateRoute path="/bookings" exact component={Bookings} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
     </Router>
 
   );
